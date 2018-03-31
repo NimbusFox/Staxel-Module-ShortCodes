a6fc10ce35b2e0f9c3cd6935065f5995bae6fc92
NimbusFox <simonbailey10@hotmail.com>
2018-03-31 22:04:40 +0100

    Port
    Moved over Dictionary based items
    
    Copied over modules from the core mod to make this module independant

---
+++ DictionaryData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusFox.Module.ShortCodes {
+    public class DictionaryData {
+        public string SubVariantKey { get; set; }
+        public List<DictionaryData> SubVariants { get; set; }
+        public Dictionary<string, string> Variants { get; set; }
+
+        public DictionaryData() {
+            Variants = new Dictionary<string, string>();
+            SubVariants = new List<DictionaryData>();
+        }
+    }
+}
